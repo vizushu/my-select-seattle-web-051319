6e848b95f56d7a426233383e9a263c4fba3ef4ac
beneficial-abstraction-6109 <me@renierdelacruz.com>
2019-05-06 01:15:01 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,9 +1,11 @@
 def my_select(collection)
-  i = 0
-  new_collection = []
-  while collection.length > i 
-  new_collection << yield(collection[i])
-  i += 1 
+ arr = []
+ i = 0
+ len = collection.length
+ while i < len
+   item = yield collection[i]
+   arr << collection[i] if item
+   i+=1
  end
- new_collection
-end
+ arr
+end	end
