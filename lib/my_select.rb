b933e522d1712025184a28273aad066078390fa3
beneficial-abstraction-6109 <me@renierdelacruz.com>
2019-05-06 01:15:21 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,11 @@
 def my_select(collection)
- # your code here!
+ arr = []
+ i = 0
+ len = collection.length
+ while i < len
+   item = yield collection[i]
+   arr << collection[i] if item
+   i+=1
+ end
+ arr
 end
