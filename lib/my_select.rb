e246af1a757fc9c08aaf9b783ca1b0a24cfdf44f
beneficial-abstraction-6109 <me@renierdelacruz.com>
2019-05-06 01:12:45 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,9 @@
 def my_select(collection)
- # your code here!
+  i = 0
+  new_collection = []
+  while collection.length > i 
+  new_collection << yield(collection[i])
+  i += 1 
+ end
+ new_collection
 end
